8f41f7df7ae16ced6efca90fdcbb826c2df5d611
ajy0714 <ajy0714@naver.com>
2022-07-06 15:53:46 +0900

    17.7.2 useDispatch를 사용하여 액션 디스패치하기

--- chapter17/src/containers/CounterContainer.js
+++ chapter17/src/containers/CounterContainer.js
@@ -1,6 +1,7 @@
-import { useSelector } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
-import { increase, decrease } from '../modules/counter'
+import { increase, decrease } from '../modules/counter';
 
 // const CounterContainer = ({ number, increase, decrease }) => {
 //     return (
@@ -10,8 +11,13 @@ import { increase, decrease } from '../modules/counter'
 
 const CounterContainer = () => {
     const number = useSelector(state => state.counter.number);
-    return <Counter number={number} />;
-}
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+    return (
+        <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease}/>
+    );
+};
 
 export default CounterContainer;
 
